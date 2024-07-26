75d4085bf69264e13dbabe5ad37f0a8fc74ec06c
Yash <jermpro0@gmail.com>
2024-07-26 12:21:51 -0400

    prime react and props added

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import axiosDB from "../axios";
 import { Link } from "react-router-dom";
 import { ManagerNav } from "./Manager/ManagerNav";
-import { Rewards } from "./Manager/Rewards";
 import { useDispatch, useSelector } from "react-redux";
 import { getEmployee } from "../store/action/employee";
+import { EmpSuper } from "./Manager/EmpSuper";
 
 export const Manager = () => {
     const [manager, setManager] = useState({});
@@ -38,27 +38,31 @@ export const Manager = () => {
 
     return (
         <>
-        <ManagerNav />
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-5">
-            <div className="bg-white rounded-lg shadow-lg p-8 max-w-lg w-full">
+        <ManagerNav employees={list} />
+        <div className="flex flex-col items-center justify-center bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-5">
+            <div className="bg-white mt-5 rounded-lg shadow-lg p-8 max-w-lg w-full">
                 <h1 className="text-4xl font-bold text-center text-purple-700 mb-6">Welcome, {manager.name}</h1>
                 <div className="text-center m-4 flex flex-col">
-                <Link to="/Manager/view" className="m-5 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded">
-                    View Employees under Supervison
-                </Link>
                 <Link to="/Manager/history" className="m-5 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
                     View History of Points Rewarded 
                 </Link>
                 <Link to="/Manager/view" className="m-5 bg-green-500 hover:bg-green-700 text-white py-2 px-4 rounded">
                     Reward Employees
                 </Link>
+
+                
                 
                 </div>
+               
             </div>
 
-            <Rewards />
+             
+
 
             
+        </div>
+        <div>
+            <EmpSuper employeesList={list} />
         </div>
         </>
     );
--- src/components/Manager/EmpSuper.jsx
+++ src/components/Manager/EmpSuper.jsx
@@ -1,16 +1,18 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axiosDB from "../../axios";
-import { ManagerNav } from "./ManagerNav";
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import 'primereact/resources/themes/saga-blue/theme.css';  // Choose your theme
+import 'primereact/resources/primereact.min.css';
 
-export const EmpSuper = () => {
-    const [employees, setEmployees] = useState([]);
+export const EmpSuper = ({ employeesList }) => {
+    const [employees, setEmployees] = useState(employeesList);
     const [selectedEmployee, setSelectedEmployee] = useState(null);
     const [points, setPoints] = useState("");
     const [review, setReview] = useState("");
-
-    useEffect(() => {
-        getEmps();
-    }, []);
+    const [visible, setVisible] = useState(false);
 
     const getEmps = async () => {
         const resp = await axiosDB.get(`/Manager/empSuper/${localStorage.getItem('id')}`, {
@@ -25,6 +27,7 @@ export const EmpSuper = () => {
     const handleEmployeeClick = (employee) => {
         setSelectedEmployee(employee);
         setPoints(employee.points);
+        setVisible(true);  // Show the dialog
     };
 
     const handleSubmit = async (e) => {
@@ -44,6 +47,7 @@ export const EmpSuper = () => {
             setPoints("");  // Clear points
             setReview("");  // Clear review
             getEmps();
+            setVisible(false);  // Hide the dialog
 
         } catch (error) {
             console.error("Error updating employee:", error);
@@ -51,109 +55,82 @@ export const EmpSuper = () => {
         }
     };
 
+    const actionBodyTemplate = (rowData) => {
+        return (
+            <Button className="flex flex-col bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" label="Review & Award" icon="pi pi-pencil" onClick={() => handleEmployeeClick(rowData)} />
+        );
+    };
+
     return (
         <>
-        <ManagerNav/>
-        <div className="flex flex-col items-center justify-start min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-5">
-            {selectedEmployee ? (
-                <div className="w-full px-6 py-8 bg-white rounded-lg shadow-md mt-10">
-                    <p className="font-bold text-black text-5xl">Award Employee Points</p>
-                    <form onSubmit={handleSubmit} className="m-5 mt-8">
-                        <div className="mt-4">
-                            <label htmlFor="name" className="block text-left text-md text-gray-700">Employee Name</label>
-                            <input
-                                id="name"
-                                type="text"
-                                className="w-full bg-gray-700  text-white px-4 py-2 mt-2 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none"
-                                value={selectedEmployee.name}
-                                readOnly
-                            />
-                        </div>
-
-                        <div className="mt-4">
-                        <label htmlFor="points" className="block text-left text-md text-gray-700">Award Points</label>
-                        <select
-                            id="manager"
-                            className="w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border border-gray-300 rounded-md focus:border-indigo-500 focus:bg-white focus:outline-none"
-                            value={points}
-                            onChange={(e) => setPoints(e.target.value)}
-                            required
-                        >
-                            <option value="">Select Number of Points to Award</option>
-                            <option value="100">200</option>
-                            <option value="500">500</option>
-                            <option value="1000">1000</option>
-                            <option value="1500">1500</option>
-                            <option value="2000">2000</option>
-                            
-                        </select>
-                        </div>
-
-                        <div className="mt-4">
-                            <label htmlFor="review" className="block text-left text-md text-gray-700">Review</label>
-                            <textarea
-                                id="review"
-                                className="w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border border-gray-300 rounded-md focus:border-indigo-500 focus:bg-white focus:outline-none"
-                                value={review}
-                                onChange={(e) => setReview(e.target.value)}
-                                required
-                            />
-                        </div>
-
-                        <button
-                            type="submit"
-                            className="w-full px-4 py-2 mt-6 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
-                        >
-                            Update
-                        </button>
-                    </form>
+            <div className="flex flex-col items-center justify-start min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 p-5">
+                <p className="text-white text-2xl font-bold m-5">Employees under Supervision</p>
+                <p className="text-white text-md">Select Employee to Reward and Review</p>
+                <div className="bg-white rounded-lg mt-5 shadow-lg p-8 w-full">
+                    <DataTable value={employees} stripedRows tableStyle={{ minWidth: '50rem' }}>
+                        <Column className="border 1" field="name" header="Employee" body={(rowData) => <div className="text-lg leading-5">{rowData.name}</div>} />
+                        <Column className="border 1" field="jobtype" header="Job Type" body={(rowData) => <div className="capitalize text-md leading-5">{String(rowData.jobtype).replace("_", " ")}</div>} />
+                        <Column className="border 1" field="salary" header="Salary" body={(rowData) => <div className="text-md leading-5">${rowData.salary}</div>} />
+                        <Column className="border 1" field="points" header="Points" body={(rowData) => <div className="text-md leading-5">{rowData.points}</div>} />
+                        <Column className="border 1 flex justify-center items-center" body={actionBodyTemplate} header="Actions" />
+                    </DataTable>
                 </div>
-            ) : (
-                <>
-                    <p className="text-white text-5xl font-bold m-5">Employees under Supervision</p>
-                    <p className="text-white text-xl">Select Employee to Reward and Review</p>
-                    <div className="bg-white rounded-lg mt-5 shadow-lg p-8 w-full">
-                        
-                        <div className="flex justify-center items-center w-full">
-                            <table className="min-w-full bg-white rounded-t-lg m-2">
-                                <thead>
-                                    <tr>
-                                        <th className="px-6 py-5 border-b-2 border-gray-300 text-2xl text-center leading-4 text-gray-700">Employee</th>
-                                        <th className="px-6 py-5 border-b-2 border-gray-300 text-2xl text-center leading-4 text-gray-700">Job Type</th>
-                                        <th className="px-6 py-5 border-b-2 border-gray-300 text-2xl text-center leading-4 text-gray-700">Salary</th>
-                                        <th className="px-6 py-5 border-b-2 border-gray-300 text-2xl text-center leading-4 text-gray-700">Points</th>
-                                        
-                                    </tr>
-                                </thead>
-                                <tbody className="bg-white border-none">
-                                    {employees.map((employee) => (
-                                        <tr key={employee.id} onClick={() => handleEmployeeClick(employee)} className="cursor-pointer border border-gray-400 rounded-md border-gray-700 hover:shadow-md hover:border-white">
-                                            <td className="px-6 py-4 whitespace-no-wrap bg-gray-700 border-gray-500">
-                                                <div className="flex items-center justify-center">
-                                                    <div>
-                                                        <div className="text-white text-lg leading-5">{employee.name}</div>
-                                                    </div>
-                                                </div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-no-wrap bg-gray-700 border-gray-500">
-                                                <div className="capitalize text-md leading-5 text-white m-2">{String(employee.jobtype).replace("_"," ")}</div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-no-wrap bg-gray-700 border-gray-500">
-                                                <div className="text-md text-white leading-5 m-2">$ {employee.salary}</div>
-                                            </td>
-                                            <td className="px-6 py-4 whitespace-no-wrap bg-gray-700 border-gray-500">
-                                                <div className="text-md leading-5 text-white m-2">{employee.points}</div>
-                                            </td>
-                                            
-                                        </tr>
-                                    ))}
-                                </tbody>
-                            </table>
-                        </div>
+            </div>
+
+            <Dialog header="Award Employee Points" visible={visible} style={{ width: '50vw' }} onHide={() => setVisible(false)}>
+                {selectedEmployee && (
+                    <div className="w-full px-6 py-2 bg-white rounded-lg">
+                        <form onSubmit={handleSubmit} className="m-5 mt-8">
+                            <div className="mt-4">
+                                <label htmlFor="name" className="block text-left text-md text-gray-700">Employee Name</label>
+                                <input
+                                    id="name"
+                                    type="text"
+                                    className="w-full bg-gray-700  text-white px-4 py-2 mt-2 border border-gray-300 rounded-md focus:border-indigo-500 focus:outline-none"
+                                    value={selectedEmployee.name}
+                                    readOnly
+                                />
+                            </div>
+
+                            <div className="mt-4">
+                                <label htmlFor="points" className="block text-left text-md text-gray-700">Award Points</label>
+                                <select
+                                    id="points"
+                                    className="w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border border-gray-300 rounded-md focus:border-indigo-500 focus:bg-white focus:outline-none"
+                                    value={points}
+                                    onChange={(e) => setPoints(e.target.value)}
+                                    required
+                                >
+                                    <option value="">Select Number of Points to Award</option>
+                                    <option value="200">200</option>
+                                    <option value="500">500</option>
+                                    <option value="1000">1000</option>
+                                    <option value="1500">1500</option>
+                                    <option value="2000">2000</option>
+                                </select>
+                            </div>
+
+                            <div className="mt-4">
+                                <label htmlFor="review" className="block text-left text-md text-gray-700">Review</label>
+                                <textarea
+                                    id="review"
+                                    className="w-full px-4 py-2 mt-2 text-gray-700 bg-gray-200 border border-gray-300 rounded-md focus:border-indigo-500 focus:bg-white focus:outline-none"
+                                    value={review}
+                                    onChange={(e) => setReview(e.target.value)}
+                                    required
+                                />
+                            </div>
+
+                            <button
+                                type="submit"
+                                className="w-full px-4 py-2 mt-6 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"
+                            >
+                                Update
+                            </button>
+                        </form>
                     </div>
-                </>
-            )}
-        </div>
+                )}
+            </Dialog>
         </>
     );
 };
